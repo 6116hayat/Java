4153cd81a619bfcb14aa2f7bb8d111e09856d646
Umar Hayat <umarhayat9718@gmail.com>
2024-08-13 17:49:40 +0530

    13/08/2024

---
+++ Online_Sources/objectsANDclasses/main.java
@@ -0,0 +1,27 @@
+package Online_Sources.objectsANDclasses;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        
+        car car1 = new car("Mercedes", "G-Wagon",2023 ,"White", 25000000 );
+
+        car car2 = new car("Lamborgini", "Urus",2022 ,"Black", 15000000 );
+        
+
+        // car car1 = new car();
+        // car car2 = new car();
+
+        //displaying details
+        car1.displayDetails();
+        car2.displayDetails();
+
+        // method drive
+        car1.drive();
+        car2.drive();
+    }
+}
---
+++ Projects/E_NBA_Draft/measurements_taking.java
@@ -0,0 +1,22 @@
+package Projects.E_NBA_Draft;
+
+        
+        // Creating the NBA Draft of players using the given data criteria
+        // 1.Height 2.Wingspan 3.Weight 4.Body fat 5.Age
+
+        // BMI formula 
+        // BMI = weight (kg) ÷ [height] * [height] (meters)
+        
+        // Find the body fat percentage using the BMI formula 
+        // (1.20 * BMI) + (0.23 * Age) - 16.2 = Body fat percentage 
+
+    public class measurements_taking {
+        public double player_height;
+        public double player_wingspan;
+        public double player_weight;
+        public double player_Bodyfat;
+        public double player_age;
+
+    }
+
+
