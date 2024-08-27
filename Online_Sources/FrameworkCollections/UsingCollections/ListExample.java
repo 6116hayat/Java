37a1b09bf7c9d53ab61fb3f8be3ef710eb216153
Umar Hayat <umarhayat9718@gmail.com>
2024-08-27 17:49:48 +0530

    updating

--- Online_Sources/FrameworkCollections/UsingCollections/ListExample.java
+++ Online_Sources/FrameworkCollections/UsingCollections/ListExample.java
@@ -2,163 +2,167 @@ package FrameworkCollections.UsingCollections;
 
 import java.util.*;
 
-/* 
+/*
+ * 
+ * // Class to add elements
+ * class AddElements {
+ * public static void main(String args[]) {
+ * 
+ * List<String> l1 = new ArrayList<>();
+ * 
+ * l1.add("Umar");
+ * l1.add("Hayat");
+ * 
+ * System.out.println(l1);
+ * }
+ * }
+ * 
+ */
 
-// Class to add elements
-class AddElements {
-    public static void main(String args[]) {
+/*
+ * 
+ * // Class to update Elements
+ * class UpdatingElements {
+ * public static void main(String[] args) {
+ * 
+ * List<String> l2 = new ArrayList<>();
+ * 
+ * l2.add("Danish");
+ * l2.add("Hayat");
+ * 
+ * // Display list before any update
+ * System.out.println("Original List: " + l2);
+ * 
+ * // After update
+ * l2.set(0, "Umar");
+ * 
+ * System.out.println("Updated List: " + l2);
+ * }
+ * }
+ * 
+ */
 
-        List<String> l1 = new ArrayList<>();
+/*
+ * 
+ * // Class to searching elements
+ * class SearchingList {
+ * public static void main(String[] args) {
+ * List<Integer> num = new ArrayList<>();
+ * 
+ * num.add(1);
+ * num.add(2);
+ * num.add(3);
+ * num.add(4);
+ * num.add(5);
+ * 
+ * System.out.println(num);
+ * 
+ * int index = num.indexOf(1);
+ * System.out.println("Index " + index + " has : " + index);
+ * 
+ * int lastIndex = num.lastIndexOf(5);
+ * System.out.println("The Last index has :" + lastIndex);
+ * }
+ * }
+ * 
+ */
 
-        l1.add("Umar");
-        l1.add("Hayat");
+/*
+ * 
+ * // Class to remove Elements
+ * class RemoveElements {
+ * public static void main(String[] args) {
+ * 
+ * List<String> l3 = new ArrayList<>();
+ * 
+ * l3.add("Umar");
+ * l3.add("Hayat");
+ * 
+ * System.out.println("Orignal List :" + l3);
+ * 
+ * l3.add(0, "Danish");
+ * 
+ * l3.remove(1);
+ * 
+ * System.out.println("List After the removal :" + l3);
+ * 
+ * }
+ * }
+ * 
+ */
 
-        System.out.println(l1);
-    }
-}
+/*
+ * 
+ * // Class for Accessing Elements
+ * class AccessElements {
+ * public static void main(String[] args) {
+ * 
+ * List<String> l4 = new ArrayList<>();
+ * 
+ * l4.add("Sheikh");
+ * l4.add("Meraj");
+ * l4.add("Ahmed");
+ * 
+ * String first = l4.get(0);
+ * String second = l4.get(1);
+ * String third = l4.get(2);
+ * 
+ * System.out.println("First Element : " + first);
+ * System.out.println("Second Element : " + second);
+ * System.out.println("Third Element : " + third);
+ * }
+ * }
+ * 
+ */
 
-*/
+/*
+ * 
+ * // Class For Checking if element is present or not
+ * class CheckingElements {
+ * public static void main(String[] args) {
+ * 
+ * List<String> l5 = new ArrayList<>();
+ * 
+ * l5.add("Sheikh");
+ * l5.add("Meraj");
+ * l5.add("Ahmed");
+ * 
+ * System.out.println(l5);
+ * 
+ * boolean isPresent = l5.contains("Danish");
+ * 
+ * System.out.println("Is Danish  present in list ? " + isPresent);
+ * }
+ * }
+ * 
+ */
 
-/* 
-
-// Class to update Elements
-class UpdatingElements {
-    public static void main(String[] args) {
-
-        List<String> l2 = new ArrayList<>();
-
-        l2.add("Danish");
-        l2.add("Hayat");
-
-        // Display list before any update
-        System.out.println("Original List: " + l2);
-
-        // After update
-        l2.set(0, "Umar");
-
-        System.out.println("Updated List: " + l2);
-    }
-}
-
-*/
-
-/* 
-
-// Class to searching elements
-class SearchingList {
-    public static void main(String[] args) {
-        List<Integer> num = new ArrayList<>();
-
-        num.add(1);
-        num.add(2);
-        num.add(3);
-        num.add(4);
-        num.add(5);
-
-        System.out.println(num);
-
-        int index = num.indexOf(1);
-        System.out.println("Index " + index + " has : " + index);
-
-        int lastIndex = num.lastIndexOf(5);
-        System.out.println("The Last index has :" + lastIndex);
-    }
-}
-
-*/
-
-/* 
-
-// Class to remove Elements
-class RemoveElements {
-    public static void main(String[] args) {
-
-        List<String> l3 = new ArrayList<>();
-
-        l3.add("Umar");
-        l3.add("Hayat");
-
-        System.out.println("Orignal List :" + l3);
-
-        l3.add(0, "Danish");
-
-        l3.remove(1);
-
-        System.out.println("List After the removal :" + l3);
-
-    }
-}
-
-*/
-
-/* 
-
-// Class for Accessing Elements
-class AccessElements {
-    public static void main(String[] args) {
-
-        List<String> l4 = new ArrayList<>();
-
-        l4.add("Sheikh");
-        l4.add("Meraj");
-        l4.add("Ahmed");
-
-        String first = l4.get(0);
-        String second = l4.get(1);
-        String third = l4.get(2);
-
-        System.out.println("First Element : " + first);
-        System.out.println("Second Element : " + second);
-        System.out.println("Third Element : " + third);
-    }
-}
-
-*/
-
-/* 
-
-// Class For Checking if element is present or not
-class CheckingElements {
-    public static void main(String[] args) {
-
-        List<String> l5 = new ArrayList<>();
-
-        l5.add("Sheikh");
-        l5.add("Meraj");
-        l5.add("Ahmed");
-
-        System.out.println(l5);
-
-        boolean isPresent = l5.contains("Danish");
-
-        System.out.println("Is Danish  present in list ? " + isPresent);
-    }
-}
-
-*/
-
-// Iterating Over the List:
-class IteratingClass {
-    public static void main(String[] args) {
-
-        List<String> l6 = new ArrayList<>();
-
-        l6.add("My");
-        l6.add("name");
-        l6.add("is");
-        l6.add("Umar");
-        l6.add("Hayat");
-
-        // Using for Loop for Iteration
-        for (int i = 0; i < l6.size(); i++) {
-            System.out.println(l6.get(i) + " ");
-        }
-
-        System.out.println();
-
-        // Using for-each loop for Iteration
-        for (String str : l6) {
-            System.out.println(str + " ");
-        }
-    }
-}
+/*
+ * 
+ * // Iterating Over the List:
+ * class IteratingClass {
+ * public static void main(String[] args) {
+ * 
+ * List<String> l6 = new ArrayList<>();
+ * 
+ * l6.add("My");
+ * l6.add("name");
+ * l6.add("is");
+ * l6.add("Umar");
+ * l6.add("Hayat");
+ * 
+ * // Using for Loop for Iteration
+ * for (int i = 0; i < l6.size(); i++) {
+ * System.out.println(l6.get(i) + " ");
+ * }
+ * 
+ * System.out.println();
+ * 
+ * // Using for-each loop for Iteration
+ * for (String str : l6) {
+ * System.out.println(str + " ");
+ * }
+ * }
+ * }
+ * 
+ */
