ff91dcccc4420ca0fbb67b44f197ca8d11c12e91
Umar Hayat <umarhayat9718@gmail.com>
2024-08-28 21:57:19 +0530

    Uploading new learning its Late ik but better late than never

---
+++ Online_Sources/FrameworkCollections/UsingCollections/ListExample2.java
@@ -0,0 +1,40 @@
+package FrameworkCollections.UsingCollections;
+
+import java.util.*;
+
+// List Interfaces :
+/*
+ * We will proceed in :-
+ * 1. Arraylist
+ * 2. Vector
+ * 3. Stack
+ * 4. LinkedList
+ */
+
+/*
+ * 
+ * class ArrayListExample {
+ * public static void main(String[] args) {
+ * 
+ * int n = 5; // Size of ArrayList
+ * 
+ * // Declaring the list with size of n;
+ * List<Integer> arrayList = new ArrayList<Integer>(n);
+ * 
+ * // Appending the new elements
+ * for (int i = 1; i <= n; i++) {
+ * arrayList.add(i);
+ * }
+ * 
+ * // Printing elements
+ * System.out.println(arrayList);
+ * 
+ * // Removing elements at index 3
+ * arrayList.remove(3);
+ * 
+ * System.out.println(arrayList);
+ * 
+ * }
+ * }
+ * 
+ */
