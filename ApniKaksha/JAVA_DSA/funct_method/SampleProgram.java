6562dfecfbc13b997803f147deb5ef45e4c29998
Umar Hayat <umarhayat9718@gmail.com>
2024-09-12 17:24:36 +0530

    functions methods

--- ApniKaksha/JAVA_DSA/funct_method/SampleProgram.java
+++ ApniKaksha/JAVA_DSA/funct_method/SampleProgram.java
@@ -4,29 +4,87 @@ import java.util.*;
 
 public class SampleProgram {
 
-    // Hello world function
-    public static void printHelloWorld() {
-        System.out.println("Hello World");
+    /*
+     * // Hello world function
+     * public static void printHelloWorld() {
+     * System.out.println("Hello World");
+     * }
+     * 
+     * // parameter function
+     * public static int sum(int a, int b) { // parameters / formal parameters
+     * return a + b;
+     * }
+     * 
+     * // function for swap values
+     * public static void swap(int num1, int num2) {
+     * int temp = num1;
+     * num1 = num2;
+     * num2 = temp;
+     * System.out.println("After Swapping");
+     * System.out.println("Number 1 = " + num1 + "\n" + "Number 2 = " + num2);
+     * }
+     * 
+     * // function for products
+     * public static int multiply(int a, int b) {
+     * return a * b;
+     * }
+     * 
+     * // factorial of a number
+     * public static int factorial(int n) {
+     * int fact = 1;
+     * for (int i = 1; i <= n; i++) {
+     * fact *= i;
+     * }
+     * return fact;
+     * }
+     * 
+     * // binomial cofficient
+     * /*
+     * Binomial coefficient = {(f)!} / {(r)! * (n-r)!}
+     */
+
+    /*
+     * public static int binCoeff(int n, int r) {
+     * 
+     * int fact_n = factorial(n);
+     * int fact_r = factorial(r);
+     * int fact_nmr = factorial(n - r);
+     * 
+     * int binCoeff = fact_n / (fact_r * fact_nmr);
+     * return binCoeff;
+     * }
+     * 
+     * // Is number a prime or not (Important)
+     * public static boolean isPrime(int num) {
+     * boolean isPrime = true;
+     * for (int i = 2; i <= num - 1; i++) {
+     * if (num % i == 0) { // completely divides
+     * isPrime = false;
+     * break;
+     * }
+     * }
+     * return isPrime;
+     * }
+     */
+
+    // different way for isPrime
+    public static boolean isPrime(int n) {
+
+        // special case
+        if (n == 2) {
+            return true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    // parameter function
-    public static int sum(int a, int b) { // parameters / formal parameters
-        return a + b;
-    }
-
-    // function for swap values
-    public static void swap(int num1, int num2) {
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
-        System.out.println("After Swapping");
-        System.out.println("Number 1 = " + num1 + "\n" + "Number 2 = " + num2);
-    }
-
-    // function for products
-    public static int multiply(int a, int b) {
-        return a * b;
-    }
+    // prime in range
+    // public
 
     public static void main(String[] args) {
         // calling the Scanner
@@ -68,11 +126,38 @@ public class SampleProgram {
          * System.out.println("Number 1 = " + num1 + "\n" + "Number 2 = " + num2);
          */
 
-        // product of two numbers
-        System.out.println("Enter the 2 numbers ");
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int product = multiply(a, b);
-        System.out.println("Product of 2 numbbers : " + product);
+        /*
+         * // product of two numbers
+         * System.out.println("Enter the 2 numbers ");
+         * int a = in.nextInt();
+         * int b = in.nextInt();
+         * int product = multiply(a, b);
+         * System.out.println("Product of 2 numbbers : " + product);
+         */
+
+        /*
+         * // factorial of a number
+         * System.out.println("Enter the number for factorial ");
+         * int a = in.nextInt();
+         * int f = factorial(a);
+         * System.out.println("The Factorial of number " + a + " = " + f);
+         */
+
+        /*
+         * // binomial coefficient
+         * System.out.println("Binomial coefficient = " + binCoeff(5, 2));
+         */
+
+        /*
+         * // isPrime
+         * System.out.println("Enter the number to check for Prime");
+         * int num = in.nextInt();
+         * System.out.println("Is the number Prime : " + isPrime(num));
+         */
+
+        System.out.println("Enter the number to check for Prime");
+        int n = in.nextInt();
+        System.out.println("Is the number Prime : " + isPrime(n));
+
     }
 }
